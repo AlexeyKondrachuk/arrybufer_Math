2a09d88c7485bcb7c702fe1a0c685b0474f0163d
Alexey.K <lexxtlt@gmail.com>
2024-01-23 16:56:22 +0400

    correct test

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,18 @@
 // TODO: write your code here
 export function getBuffer() {
-	const data = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+	  const data = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
 	return ((input) => {
 	  const buffer = new ArrayBuffer(data.length * 2);
 	  const bufferView = new Uint16Array(buffer);
-	  for (let i = 0; i < input.length; i += 1) {
-		bufferView[i] = input.charCodeAt(i);
-	  }
-	  return buffer;
+	for (let i = 0; i < input.length; i += 1) {
+	  bufferView[i] = input.charCodeAt(i);
+    }
+	return buffer;
 	})(data);
-  }
+    }
   
-  export class ArrayBufferConverter {
-	constructor() {
+export class ArrayBufferConverter {
+	  constructor() {
 	  this.buffer = null;
 	}
   
